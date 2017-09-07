6d042f54d7a62394c1e7d6dc315f22a20250aee0
HQ <hongsenqin@gmail.com>
2017-09-07 14:01:20 -0500

    update

---
+++ square/square.cpp
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/02/2016 07:51:20 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  HQ (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+int main(){
+    std::cout<<"hello\nenter a number that you would like to square\nint=";
+    int x;
+    std::cin>>x;
+    std::cout<<x<<" squared is "<<x*x<<"\n";
+    
+    
+    
+    
+    return 0;
+
+}
+
+
